e3545dc35f456971ad8f085f5810952972bf3bfb
Joel Palmer <joelpalmer@gmail.com>
2019-06-25 13:32:54 -0500

    implement basic connection & update readme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate subprocess;
+use std::env;
+use std::io::{BufRead, BufReader};
+use subprocess::Exec;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let ip = &args[1];
+    let x = Exec::cmd("adb").arg("connect").arg(ip).join();
 }
